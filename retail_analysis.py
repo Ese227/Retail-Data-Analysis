525819527703fe3e6d06a2180c56bb76291034b5
Ese227 <abueseosa26@gmail.com>
2026-02-26 19:41:02 +0000

    Add project files.

---
+++ retail_analysis.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("sales.csv")
+
+# Clean
+df.dropna(inplace=True)
+
+# Revenue by product
+product_sales = df.groupby("Item_Type")["Item_Outlet_Sales"].sum()
+
+print(product_sales)
+
+# Plot
+product_sales.sort_values().plot(kind="barh")
+plt.title("Sales by Product Type")
+plt.show()
+
+# Save summary
+product_sales.to_csv("sales_summary.csv")
